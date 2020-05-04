d7c6f3eeb8333d920770bb41de8c6e6980aa943f
Pierre Rouanet <pierre.rouanet@gmail.com>
2020-05-04 10:03:46 +0200

    Add prefabs and script for reachy and support.

---
+++ Assets/Scripts/ReachyController.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System;
+
+namespace Reachy
+{
+    [System.Serializable]
+    public class Motor
+    {
+        public string name;
+        public GameObject gameObject;
+        public float targetPosition;
+        public float presentPosition;
+    }
+
+    [System.Serializable]
+    public struct SerializableMotor
+    {
+        public string name;
+        public float present_position;
+    }
+
+    [System.Serializable]
+    public struct SerializableState
+    {
+        public List<SerializableMotor> motors;
+        public string left_eye;
+        public string right_eye;
+    }
+
+    [System.Serializable]
+    public struct MotorCommand
+    {
+        public string name;
+        public float goal_position;
+    }
+
+    [System.Serializable]
+    public struct SerializableCommands
+    {
+        public List<MotorCommand> motors;
+    }
+
+    public class ReachyController : MonoBehaviour
+    {
+        public Motor[] motors;
+        public Camera leftEye, rightEye;
+        private Dictionary<string, Motor> name2motor;
+        private string leftEyeFrame, rightEyeFrame;
+
+        const int resWidth = 320;
+        const int resHeight = 240;
+
+        void Start()
+        {
+            name2motor = new Dictionary<string, Motor>();
+
+            for (int i = 0; i < motors.Length; i++)
+            {
+                Motor m = motors[i];
+                name2motor[m.name] = m;
+            }
+
+            leftEye.targetTexture = new RenderTexture(resWidth, resHeight, 0);
+            rightEye.targetTexture = new RenderTexture(resWidth, resHeight, 0);
+            StartCoroutine("UpdateCameraData");
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < motors.Length; i++)
+            {
+                Motor m = motors[i];
+
+                JointController joint = m.gameObject.GetComponent<JointController>();
+                joint.RotateTo(m.targetPosition);
+
+                m.presentPosition = joint.GetPresentPosition();
+            }
+        }
+
+        IEnumerator UpdateCameraData()
+        {
+            while (true)
+            {
+                yield return new WaitForEndOfFrame();
+                leftEyeFrame = GetEyeRawTextureData(leftEye);
+                rightEyeFrame = GetEyeRawTextureData(rightEye);
+            }
+        }
+
+        string GetEyeRawTextureData(Camera camera)
+        {
+            Texture2D texture = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            RenderTexture.active = camera.targetTexture;
+            texture.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            texture.Apply();
+
+            return Convert.ToBase64String(texture.EncodeToJPG());
+        }
+
+        void SetMotorTargetPosition(string motorName, float targetPosition)
+        {
+            name2motor[motorName].targetPosition = targetPosition;
+        }
+
+        public void HandleCommand(SerializableCommands commands)
+        {
+            for (int i = 0; i < commands.motors.Capacity; i++) 
+            {
+                MotorCommand cmd = commands.motors[i];
+                SetMotorTargetPosition(cmd.name, cmd.goal_position);
+            }
+        }
+
+        public SerializableState GetCurrentState()
+        {
+            
+            SerializableState currentState = new SerializableState() { 
+                motors = new List<SerializableMotor>(), 
+                left_eye=leftEyeFrame,
+                right_eye=rightEyeFrame,
+            };
+
+            for (int i = 0; i < motors.Length; i++)
+            {
+                Motor m = motors[i];
+                currentState.motors.Add(new SerializableMotor() { name=m.name,  present_position=m.presentPosition});
+            }
+
+            return currentState;
+        }
+    }
+}
---
+++ Assets/Scripts/WsIO.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NativeWebSocket;
+using Reachy;
+
+public class WsIO : MonoBehaviour
+{
+    public string url = "ws://127.0.0.1:6171";
+    public ReachyController reachy;
+
+    WebSocket websocket;
+    SerializableCommands commands;
+    bool needUpdate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Connect");
+    }
+
+    async void Connect()
+    {
+        websocket = new WebSocket(url);
+
+        websocket.OnOpen += () =>
+        {
+            Debug.Log("Connection open!");
+        };
+        websocket.OnError += (e) =>
+        {
+            Debug.Log("Error! " + e);
+        };
+
+        websocket.OnClose += (e) =>
+        {
+            Debug.Log("Connection closed!");
+        };
+
+        websocket.OnMessage += (bytes) =>
+        {
+            var message = System.Text.Encoding.UTF8.GetString(bytes);
+
+            needUpdate = true;
+            commands = JsonUtility.FromJson<SerializableCommands>(message);
+            websocket.SendText(JsonUtility.ToJson(reachy.GetCurrentState()));
+        };
+
+        await websocket.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (needUpdate)
+        {
+            reachy.HandleCommand(commands);
+            needUpdate = false;
+        }
+    }
+}
